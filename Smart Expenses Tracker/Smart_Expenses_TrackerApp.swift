ef0c498105d4d6e284e03f3bcd69672966256aab
abdulwariskhan <hellokhan.ch@gmail.com>
2024-04-04 23:29:05 +0200

    Initial Commit

---
+++ Smart Expenses Tracker/Smart_Expenses_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_Expenses_TrackerApp.swift
+//  Smart Expenses Tracker
+//
+//  Created by Khan on 04.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Smart_Expenses_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
