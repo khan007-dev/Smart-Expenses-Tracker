410396b9fe2cfa955943cca28dbfd4e892076207
abdulwariskhan <hellokhan.ch@gmail.com>
2024-04-08 11:54:40 +0200

    Added SwipAction

---
+++ Smart Expenses Tracker/View/Helper/DataFilterView.swift
@@ -0,0 +1,18 @@
+//
+//  DataFilterView.swift
+//  Smart Expenses Tracker
+//
+//  Created by Khan on 08.04.2024.
+//
+
+import SwiftUI
+
+struct DataFilterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DataFilterView()
+}
---
+++ Smart Expenses Tracker/View/Helper/TransactionCardView.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionCardView.swift
+//  Smart Expenses Tracker
+//
+//  Created by Khan on 08.04.2024.
+//
+
+import SwiftUI
+
+struct TransactionCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionCardView()
+}
