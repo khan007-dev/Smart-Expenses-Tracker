9481333ee5076eec200e7f78d01fb8993392d979
abdulwariskhan <hellokhan.ch@gmail.com>
2024-04-11 20:59:35 +0200

    Implemented Data

---
+++ Smart Expenses Tracker/View/NewExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  NewExpenseView.swift
+//  Smart Expenses Tracker
+//
+//  Created by Khan on 11.04.2024.
+//
+
+import SwiftUI
+
+struct NewExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewExpenseView()
+}
