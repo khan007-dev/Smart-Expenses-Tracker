0288f83af9110f9deb538eba62c755a1a88cbd3c
abdulwariskhan <hellokhan.ch@gmail.com>
2024-04-13 00:40:50 +0200

    Completed Charts

---
+++ Smart Expenses Tracker/Model/ChartModel.swift
@@ -0,0 +1,8 @@
+//
+//  ChartModel.swift
+//  Smart Expenses Tracker
+//
+//  Created by Khan on 12.04.2024.
+//
+
+import Foundation
---
+++ StateCardView/StatsCardViewBundle.swift
@@ -0,0 +1,16 @@
+//
+//  StatsCardViewBundle.swift
+//  StatsCardView
+//
+//  Created by Balaji Venkatesh on 19/12/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StatsCardViewBundle: WidgetBundle {
+    var body: some Widget {
+        StatsCardView()
+    }
+}
